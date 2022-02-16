5124127e0b015b9a61b1b0efc10e14cf5919aa33
scrof90 <asknyshev@gmail.com>
2022-02-16 03:17:20 +0200

    add exercises 3.1.-3.22.

---
+++ part3/phonebook-app/README.md
@@ -0,0 +1,7 @@
+# Full Stack open 2021
+
+## 3.12.-3.22.: Phonebook Fullstack App
+
+[App on Heroku](https://serene-falls-56324.herokuapp.com/)
+
+Time taken: 5 hours
---
+++ part3/phonebook-app/models/person.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, minLength: 3, required: [true, 'User name required'] },
+  number: {
+    type: String,
+    minLength: 9,
+    validate: {
+      validator: (v) => /\d{2,3}-\d/.test(v),
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, 'User phone number required'],
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', personSchema)
